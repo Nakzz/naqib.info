0c335398d8fb37965fc31fd47f51522801d4ea0f
Nakzz <naqibprio10@gmail.com>
2021-03-24 21:22:12 -0500

    SSL enabled

--- client_nextjs/utils/hostname.js
+++ client_nextjs/utils/hostname.js
@@ -19,9 +19,10 @@ export function cmsLinkResolver(){
 	var url =
 		process.env.NODE_ENV === "docker_production" ||
 		process.env.NODE_ENV === "production"
-			? "http://172.17.0.1:8010/admin/api"
-			: "http://192.168.1.126:5000/admin/api";
-    
+			? "https://172.17.0.1:8010/admin/api"
+			: "https://192.168.1.126:5000/admin/api";
+	
+	url = "https://192.168.1.126:5000/admin/api" //devbuild
             console.log(url);
     console.log("Client env: " + process.env.NODE_ENV);
     
