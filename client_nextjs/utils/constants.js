787b733ca0f32940d57e1c37895e0072343425eb
AJ <naqibprio10@gmail.com>
2022-01-28 02:25:42 +0000

    Ref: added constant references for strings

---
+++ client_nextjs/utils/constants.js
@@ -0,0 +1,7 @@
+export const BLOG_URL = 'https://blog.naqib.info';
+export const LINKEDIN_URL = "https://www.linkedin.com/in/ajmain/"
+export const GITHUB_URL = "https://github.com/Nakzz/"
+
+export const EMAIL = "aj@naqib.info"
+
+export const DOMAIN = "https://naqib.info"
