cb6a3e292da8c535cf881cf5661b638c829e49e0
AJ <naqibprio10@gmail.com>
2022-01-28 04:09:33 +0000

    build: tried optimising build, didnt work

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 # docker-compose up -d --force-recreate client
 # docker logs $(docker ps -aqf "name=client_nextjs")
 
-docker-compose build cms
-docker-compose up -d --force-recreate cms
-docker logs $(docker ps -aqf "name=cms_keystone")
+docker-compose build client
+docker-compose up -d --force-recreate client
+docker logs $(docker ps -aqf "name=client")
 
 
 # docker run -it $(docker ps -aqf "name=cms_keystone") /bin/sh
