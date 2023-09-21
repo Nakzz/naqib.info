88056b8032f5ded8aaad64225da4519f7dc6f392
AJ <naqibprio10@gmail.com>
2023-09-21 12:30:16 -0400

    code cleanup

--- apps/client/server.mjs
+++ apps/client/server.mjs
@@ -2,26 +2,13 @@ import express from "express";
 import next from "next";
 import path from "path";
 import bodyParser from "body-parser";
-// import stripe from "stripe"(keys.stripeSecretKey);
-// import fs from "fs";
 import http from "http";
-// import https from "https";
 import compression from "compression";
 import helmet from "helmet";
 import sitemap from "nextjs-sitemap-generator";
 
 let __dirname = path.resolve();
 
-// import keys from "./server/config/keys";
-// var privateKey = fs.readFileSync(
-// 	"/data/naqib.info_static_content/cert/privkey.pem",
-// 	"utf8"
-// );
-// var certificate = fs.readFileSync(
-// 	"/data/naqib.info_static_content/cert/fullchain.pem",
-// 	"utf8"
-// );
-// var credentials = { key: privateKey, cert: certificate };
 
 const dev = process.env.NODE_ENV !== "production";
 
@@ -62,7 +49,7 @@ app.prepare().then(() => {
 	// https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
 	server.use(
 		"/images",
-		express.static(path.join(__dirname, "images"), {
+		express.static(path.join(__dirname, "assets/images"), {
 			maxAge: dev ? "0" : "365d",
 		})
 	);
@@ -105,29 +92,12 @@ app.prepare().then(() => {
 		return app.render(req, res, "/coming-soon"); //FOR COMING SOON PAGE REDIRECT
 	});
 
-	//NOT USING STRIPE
-	// server.post('/api/stripe/checkout', async (req, res) => {
-	//     await stripe.charges.create({
-	//         amount: req.body.amount,
-	//         currency: 'usd',
-	//         description: 'Naqib.info application React Next eCommerce + Landing Page Templates',
-	//         source: req.body.token.id
-	//     });
-	//     res.send({})
-	// });
-
 	const PORT = Number(process.env.PORT) || 7000;
 
 	var httpServer = http.createServer(server);
-	// var httpsServer = https.createServer(credentials, server);
 
 	httpServer.listen(PORT, () => {
 		// if (err) throw err;
 		console.log(`> Read on http://localhost:${PORT}`);
 	});
-
-	// httpsServer.listen(PORT + 1, () => {
-	// 	// if (err) throw err;
-	// 	console.log(`> Read on http://localhost:${PORT + 1}`);
-	// });
 });
